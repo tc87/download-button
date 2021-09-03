b599549f34dcc56e5a938f7861dfc3033adea165
tc87 <tcricks87@gmail.com>
2021-09-02 21:05:03 -0500

    Update version2.py

--- demos/version2.py
+++ demos/version2.py
@@ -6,8 +6,6 @@ import numpy as np
 def show():
     df = pd.read_csv("desktop_streamlit.csv")
 
-    st.title('Download demo set 2')
-
     st.subheader('Unicode characters in file names')
     with st.echo():
         st.download_button(
